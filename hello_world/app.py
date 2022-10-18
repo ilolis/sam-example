37411917b0a1473d511e3f55c5b8b1ebc435f6e3
Ilias Lolis <ilolis@outlook.com>
2022-10-18 21:19:19 +0300

    new pipeline

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "this is new!!",
+            "message": "this is new!!2",
             # "location": ip.text.replace("\n", "")
         }),
     }
